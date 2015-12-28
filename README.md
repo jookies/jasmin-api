24a41c3ae97eb4d24d65af346a0b1b4957e85293
Graeme Pietersz <graeme@pietersz.net>
2015-12-28 20:01:17 +0530

    Settings for minimal REST API only site
    Disable auth, sessions, admin, contenttypes and standard security etc.
    changes to settings
    
    issue #1

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # jasmin-api
 Jasmin restful management API
 
+##Installation
+
+Requires local_settings.py in jasmin_api/jasmin_api
+(same directory as settings.py) which should contain:
+
+    DEBUG = False
+    SECRET_KEY = '[some random string]'
+
 ## Dependencies
 See requirements.txt
 option libyaml would improve performance of Django REST Swagger
--- jasmin_api/jasmin_api/settings.py
+++ jasmin_api/jasmin_api/settings.py
@@ -13,38 +13,33 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+SETTINGS_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^*0a3zuxj_23&jm^^g-t$+mqn^v=od+*j5172@m6q_!#aun(18'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+#This should be OK for REST API - we are not generating URLs
+#see https://www.djangoproject.com/weblog/2013/feb/19/security/#s-issue-host-header-poisoning
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
+#    'django.contrib.admin',
+#    'django.contrib.auth',
+#    'django.contrib.contenttypes',
+#    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_swagger',
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
+#    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+#    'django.contrib.auth.middleware.AuthenticationMiddleware',
+#    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
@@ -100,3 +95,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+with open(os.path.join(SETTINGS_DIR, 'local_settings.py')) as f:
+    exec(f.read())
