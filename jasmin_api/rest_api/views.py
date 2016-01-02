f595e2b2f0327220c21a9d4344e3f678d1fb958b
Graeme Pietersz <graeme@pietersz.net>
2016-01-02 13:46:22 +0530

    Auth settings and and test view.
    Added auth and permissions settings
    Added a view that returns a simple JSON respose
    
    issue #1

--- jasmin_api/rest_api/views.py
+++ jasmin_api/rest_api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class TestView(APIView):
+    def get(self, request, format=None):
+        return Response({'result': 'ok'})
