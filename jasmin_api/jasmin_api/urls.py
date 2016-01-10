ec5bbfd9c3ca3f3418391b50d4ba1467d981162c
zoufou <fourat@gmail.com>
2016-01-10 17:29:54 +0100

    Created different modules for each jcli module (users / groups), a better way to organize codebase

--- jasmin_api/jasmin_api/urls.py
+++ jasmin_api/jasmin_api/urls.py
@@ -1,16 +1,14 @@
 from django.conf.urls import include, url
-from django.contrib import admin
 
 from rest_framework.routers import DefaultRouter
 
-from rest_api import views
+from rest_api import groups, users
 
 router = DefaultRouter()
-router.register(r'groups', views.GroupViewSet, base_name='groups')
-router.register(r'users', views.UserViewSet, base_name='users')
+router.register(r'groups', groups.GroupViewSet, base_name='groups')
+router.register(r'users', users.UserViewSet, base_name='users')
 
 urlpatterns = [
     url(r'^api/', include(router.urls)),
     url(r'^docs/', include('rest_framework_swagger.urls')),
 ]
-
--- jasmin_api/rest_api/users.py
+++ jasmin_api/rest_api/users.py
@@ -1,14 +1,11 @@
-import pexpect
-
 from django.conf import settings
 from django.http import HttpResponseBadRequest, Http404
 
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 from rest_framework.parsers import JSONParser
-from rest_framework.decorators import detail_route, list_route, parser_classes
-from rest_framework.exceptions import NotFound, APIException
+from rest_framework.decorators import detail_route, parser_classes
+from rest_framework.exceptions import APIException
 
 STANDARD_PROMPT = settings.STANDARD_PROMPT
 INTERACTIVE_PROMPT = settings.INTERACTIVE_PROMPT
@@ -52,118 +49,6 @@ def set_ikeys(telnet, keys2vals):
         #remove whitespace and return error
         raise JasminSyntaxError(" ".join(telnet.match.group(1).split()))
     return
-
-class GroupViewSet(ViewSet):
-    "ViewSet for managing *Jasmin* user groups (*not* Django auth groups)"
-    lookup_field = 'gid'
-
-    def list(self, request):
-        "List groups. No request parameters provided or required."
-        telnet = request.telnet
-        telnet.sendline('group -l')
-        telnet.expect([r'(.+)\n' + STANDARD_PROMPT])
-        result = telnet.match.group(0).strip().replace("\r", '').split("\n")
-        if len(result) < 3:
-            return Response({'groups': []})
-        groups = result[2:-2]
-        return Response(
-            {
-                'groups':
-                    [
-                        {
-                            'name': g.strip().lstrip('!#'), 'status': (
-                                'disabled' if g[1] == '!' else 'enabled'
-                            )
-                        } for g in groups
-                    ]
-            }
-        )
-
-    def create(self, request):
-        """Create a group.
-        One POST parameter required, the group identifier (a string)
-        ---
-        # YAML
-        omit_serializer: true
-        parameters:
-        - name: gid
-          description: Group identifier
-          required: true
-          type: string
-          paramType: form
-        """
-        telnet = request.telnet
-        telnet.sendline('group -a')
-        telnet.expect(r'Adding a new Group(.+)\n' + INTERACTIVE_PROMPT)
-        if not 'gid' in request.data:
-            return HttpResponseBadRequest('Missing gid (group identifier)')
-        telnet.sendline('gid ' + request.data['gid'] + '\n')
-        telnet.expect(INTERACTIVE_PROMPT)
-        telnet.sendline('ok\n')
-        matched_index = telnet.expect([
-            r'.+Successfully added(.+)\[(.+)\][\n\r]+' + STANDARD_PROMPT,
-            r'.+Error: (.+)[\n\r]+' + INTERACTIVE_PROMPT,
-            r'.+(.*)(' + INTERACTIVE_PROMPT + '|' + STANDARD_PROMPT + ')',
-        ])
-        if matched_index == 0:
-            gid = telnet.match.group(2).strip()
-            telnet.sendline('persist\n')
-            return Response({'name': gid})
-        else:
-            return HttpResponseBadRequest(telnet.match.group(1))
-
-    def simple_group_action(self, telnet, action, gid):
-        telnet.sendline('group -%s %s' % (action, gid))
-        matched_index = telnet.expect([
-            r'.+Successfully(.+)' + STANDARD_PROMPT,
-            r'.+Unknown Group: (.+)' + STANDARD_PROMPT,
-            r'.+(.*)' + STANDARD_PROMPT,
-        ])
-        if matched_index == 0:
-            telnet.sendline('persist\n')
-            return Response ({'name': gid})
-        elif matched_index == 1:
-            raise Http404()
-        else:
-            return HttpResponseBadRequest(telnet.match.group(1))
-
-    def destroy(self, request, gid):
-        """Delete a group. One parameter required, the group identifier (a string)
-
-        HTTP codes indicate result as follows
-
-        - 200: successful deletion
-        - 404: nonexistent group
-        - 400: other error
-        """
-        return self.simple_group_action(request.telnet, 'r', gid)
-
-    @detail_route(methods=['put'])
-    def enable(self, request, gid):
-        """Enable a group. One parameter required, the group identifier (a string)
-
-        HTTP codes indicate result as follows
-
-        - 200: successful deletion
-        - 404: nonexistent group
-        - 400: other error
-        """
-        return self.simple_group_action(request.telnet, 'e', gid)
-
-
-    @detail_route(methods=['put'])
-    def disable(self, request, gid):
-        """Disable a group.
-
-        One parameter required, the group identifier (a string)
-
-        HTTP codes indicate result as follows
-
-        - 200: successful deletion
-        - 404: nonexistent group
-        - 400: other error
-        """
-        return self.simple_group_action(request.telnet, 'd', gid)
 
 class UserViewSet(ViewSet):
     "ViewSet for managing *Jasmin* users (*not* Django auth users)"
